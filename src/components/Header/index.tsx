b014c06ce4af1ad4e6e7586e88ec14d70ff2c2d0
Marina Marques <marina.marqueso@outlook.com>
2022-04-08 15:01:38 -0300

    Feat: start to style the main screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 import { useContext } from "react";
 
 import UserContext from "../../context/UserContext";
+import "./style.scss";
 
 export default function Header() {
   const { username } = useContext(UserContext);
 
   return (
-    <header>
+    <header className="main-screen-header">
       <h2>CodeLeap Network</h2>
-      <h3>{username}</h3>
     </header>
   );
 }
--- src/pages/MainScreen/index.tsx
+++ src/pages/MainScreen/index.tsx
@@ -1,10 +1,11 @@
 import Header from "../../components/Header";
 import PostList from "../../components/PostList";
 import PostWriting from "../../components/PostWriting";
+import "./style.scss";
 
 function MainScreen() {
   return (
-    <div>
+    <div className="main-screen-container">
       <Header />
       <PostWriting />
       <PostList />
